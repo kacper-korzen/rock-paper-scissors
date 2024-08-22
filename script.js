6b46dbde11f9b23d824f73fa00807a92b8d35370
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-08-22 12:57:40 +0200

    add playGame function, that runs 5 round of rock, paper, scissors game

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -14,36 +13,51 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    let message = '';
-
-    switch (`${humanChoice}_${computerChoice}`) {
-        case 'rock_rock':
-        case 'paper_paper':
-        case 'scissors_scissors':
-            message = 'It\'s a tie';    
-            break;
-        case 'rock_scissors':
-        case 'paper_rock':
-        case 'scissors_paper':
-            message = `You win! ${humanChoice} beats ${computerChoice}`;
-            humanScore++;
-            break;
-        case 'scissors_rock':
-        case 'rock_paper':
-        case 'paper_scissors':
-            message = `You lose! ${computerChoice} beats ${humanChoice}`;
-            computerScore++;
-            break;
-        
-        default:
-            message = 'Error';
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+    
+        let message = '';
+    
+        switch (`${humanChoice}_${computerChoice}`) {
+            case 'rock_rock':
+            case 'paper_paper':
+            case 'scissors_scissors':
+                message = 'It\'s a tie';    
+                break;
+            case 'rock_scissors':
+            case 'paper_rock':
+            case 'scissors_paper':
+                message = `You win! ${humanChoice} beats ${computerChoice}`;
+                humanScore++;
+                break;
+            case 'scissors_rock':
+            case 'rock_paper':
+            case 'paper_scissors':
+                message = `You lose! ${computerChoice} beats ${humanChoice}`;
+                computerScore++;
+                break;
+            
+            default:
+                message = 'Error';
+        }
+    
+        console.log(message);
     }
 
-    console.log(message);
+    let humanChoice = '';
+    let computerChoice = '';
+
+    for (let i = 0; i < 5; i++) {
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+
+    }
 }
 
-playRound('scissors', 'paper');
+playGame();
